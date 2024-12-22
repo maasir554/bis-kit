161ff8626bdc68267f2d99839ee0514e41972607
MohammadMaasir <maasir554@gmail.com>
2024-12-22 20:13:15 +0530

    small ui change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { BsLinkedin, BsTwitterX, BsFacebook, BsYoutube } from "react-icons/bs"
 
 export const Footer = () => {
     return (
-        <footer className=" w-full py-8 gap-4 flex flex-row justify-center items-center bg-gradient-to-br from-themeblue/10  via-themeorange/10 to-themeblue/50 rounded-t-3xl flex-wrap">
+        <footer className=" w-full py-8 gap-4 flex flex-row justify-center items-center bg-gradient-to-br from-themeblue/10  via-themeorange/10 to-themeblue/50 flex-wrap">
            <span className="whitespace-nowrap"> Copyright &copy; 2024 Team MANIT</span>
             <span className="flex flex-row gap-2 flex-nowrap">
                 <Link target="_blank" href={"https://www.linkedin.com/company/bureau-of-indian-standards-official/"}>
